b6b34984a367d0309c836862baff1b8b83a1dd49
rin <prettysunshine468@gmail.com>
2022-07-11 23:10:08 +0600

    part2 phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/persons").then((response) => {
+      console.log("data fetched");
+      setPersons(response.data);
+    });
+  }, []);
 
   const [filterValue, setFilterValue] = useState("");
 
